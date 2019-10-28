e30d7a488d16bf9b0cea4ef2ce836fbae5c5fb10
davi ribeiro <luminuszz43@gmail.com>
2019-10-28 13:31:46 -0300

    table

--- src/pages/Header/index.js
+++ src/pages/Header/index.js
@@ -48,10 +48,17 @@ export default function Header() {
        
       </FormGroup>
       <AppBar position="static">
+        
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
+            
+            
           </IconButton>
+
+          <Typography variant="h8" >
+            Teste
+          </Typography>
           <Typography variant="h6" className={classes.title}>
             Portaria
           </Typography>
--- src/pages/Table/index.js
+++ src/pages/Table/index.js
@@ -8,7 +8,7 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import { display, textAlign } from '@material-ui/system';
 import axios from 'axios';
-
+import Button from '@material-ui/core/Button';
 
 
 
@@ -23,38 +23,28 @@ const useStyles = makeStyles({
     textAlign:'center',
     marginLeft:'140px',
     marginTop:'40px',
+    marginBottom:'25px'
     
 
   },
   table: {
     minWidth: 500,
+    textAlign:'center',
     
+  },
+  
+  input: {
+    display: 'none',
   },
 });
 
 
 
 
-
-
-function createData(name, calories, fat, carbs, protein) {
-
-
-
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
 export default function SimpleTable() {
   const [testeArray,setTeste] = useState([]);
-  function teste(){
+  useEffect((e)=>{
+  
     
       axios.get('https://swapi.co/api/planets').then((r)=>{
         console.log(r.data.results);
@@ -63,7 +53,10 @@ export default function SimpleTable() {
       
     })
 
-  }
+  
+
+  },[])
+  
   
 
   
@@ -76,24 +69,22 @@ export default function SimpleTable() {
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <button onClick={teste}></button>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+           
+          <TableCell align="right">Nome</TableCell>
+            <TableCell align="right">Empresa</TableCell>
+            <TableCell align="right">Nivel de acesso</TableCell>
+            <TableCell align="right">descrição</TableCell>
+            <TableCell align="right">ação</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {testeArray.map(row => (
             <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.name}</TableCell>
+              <TableCell align="right">{row.climate}</TableCell>
+              <TableCell align="right">{row.rotation_period}</TableCell>
+              <TableCell align="right">{row.diameter}</TableCell>
+              <TableCell align="center"><Button variant="contained" color="primary" className={classes.button}>ação</Button></TableCell>
             </TableRow>
           ))}
         </TableBody>
